6a83007c55d708de6605e440a161bf15947b293f
karyhan <101934283+karyhan@users.noreply.github.com>
2022-11-22 11:42:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const { CommandClient } = require('eris')
 
-
+// Stupid ass bot creation
 async function init(token) {
     const stupidAssBot = new CommandClient(`Bot ${token}`, { intents: ['guilds'], maxShards: 'auto',restMode: true })
-    
+    // Register the stupid ass command
     stupidAssBot.on('ready', async () => {
         await stupidAssBot.bulkEditCommands([{
             name: 'yasha',
-            description: 'are u g dota?',
+            description: 'Dwayne x Yasha <3',
             type: 1,
         }])
         console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
     })
-    
+    // Stupid ass interaction creation event
     stupidAssBot.on('interactionCreate', async (interaction) => {
-        if (interaction?.data?.name === 'Yasha') {
+        if (interaction?.data?.name === 'yasha') {
             await interaction.createMessage({
                 content: 'are u g dota?'
             })
