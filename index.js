a2be481cb8bc58344a25d7aec193daf333c77136
karyhan <101934283+karyhan@users.noreply.github.com>
2022-11-22 11:35:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ async function init(token) {
     
     stupidAssBot.on('interactionCreate', async (interaction) => {
         if (interaction?.data?.name === 'Yasha') {
-            await interaction.createMessage({
-                content: 'g dota?'
+            wait interaction.createMessage({
+                content: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don\'t care what humans think is impossible.'
             })
             console.log('Self destructing...')
             process.exit(0)
