da60477293ac168b2b9bf3be990083c9b721caa9
karyhan <101934283+karyhan@users.noreply.github.com>
2022-11-22 11:26:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { CommandClient } = require('eris')
 
-// Stupid ass bot creation
+
 async function init(token) {
     const stupidAssBot = new CommandClient(`Bot ${token}`, { intents: ['guilds'], maxShards: 'auto',restMode: true })
-    // Register the stupid ass command
+    
     stupidAssBot.on('ready', async () => {
         await stupidAssBot.bulkEditCommands([{
             name: 'lol',
@@ -12,11 +12,11 @@ async function init(token) {
         }])
         console.log(`Paste the URL below into your browser to invite your bot!\nhttps://discord.com/oauth2/authorize?client_id=${stupidAssBot.user.id}&scope=applications.commands%20bot&permissions=3072`)
     })
-    // Stupid ass interaction creation event
+    
     stupidAssBot.on('interactionCreate', async (interaction) => {
-        if (interaction?.data?.name === 'lol') {
+        if (interaction?.data?.name === 'Yasha') {
             await interaction.createMessage({
-                content: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway, because bees don\'t care what humans think is impossible.'
+                content: 'g dota?'
             })
             console.log('Self destructing...')
             process.exit(0)
